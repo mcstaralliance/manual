6c4f42e14be4796c9f82979dc2c9864be297e527
Asnxthaony <asnxthaony@outlook.com>
2020-01-31 12:43:02 +0800

    Initial commit

---
+++ man/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: '星域联盟知识库',
+  themeConfig: {
+    sidebar: [
+      ['/', '欢迎页'],
+    ],
+    lastUpdated: '最后更新 ',
+    repo: 'mcstaralliance/manual',
+    docsBranch: 'master',
+    docsDir: 'man',
+    editLinks: true,
+    docsDir: 'man',
+    editLinkText: '帮助我们完善这个页面'
+  },
+  evergreen: true
+}
