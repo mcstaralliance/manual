5dc6d076e5ffe5e16d7311f01373e3d669ff5329
LDS_XiaoYe <934664404@qq.com>
2020-10-05 20:46:35 +0800

    Update docs

--- man/.vuepress/config.js
+++ man/.vuepress/config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  title: '星域联盟知识库',
+  title: '星域联盟文档库',
   themeConfig: {
     sidebar: [
       ['/', '欢迎页'],
       ['/attention-question', '应注意的问题'],
       ['/faq', '常见问题疑难解答'],
       ['/ingame-ask-question', '游戏内容疑难解答'],
+      ['/sponsor', '星域世界赞助表']
     ],
     lastUpdated: '最后更新 ',
     repo: 'mcstaralliance/manual',
