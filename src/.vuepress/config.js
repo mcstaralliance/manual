228610453cd6c8d179ea4ef35f403cba06245aeb
LDS_XiaoYe <xiaoyecreate@outlook.com>
2024-06-15 10:26:46 +0800

    Update head

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -5,6 +5,12 @@ import { viteBundler } from '@vuepress/bundler-vite'
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '星域世界文档库',
+  head: [
+    ['link', { rel: 'canonical', href: 'https://help.mcstaralliance.com/' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }],
+    ['meta', { name: 'description', content: '星域世界的官方文档库' }],
+    ['meta', { name: 'keywords', content: '我的世界, Minecraft, 星域世界, 文档, 游戏' }],
+  ],
   theme: defaultTheme({
     navbar: [
       {
